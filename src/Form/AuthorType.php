f9cb923be03c71a24ca2b478972e87bbbfad42b6
zak39 <fotia.baptiste@hotmail.com>
2024-12-11 00:36:37 +0100

    refactor(Form): Use BirthdayType for the dateOfBirth field for author

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -6,6 +6,7 @@ use App\Entity\Author;
 use App\Entity\Book;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -19,7 +20,7 @@ class AuthorType extends AbstractType
     {
         $builder
             ->add('name', TextType::class)
-            ->add('dateOfBirth', DateType::class, [
+            ->add('dateOfBirth', BirthdayType::class, [
                 'input' => 'datetime_immutable',
                 'widget' => 'single_text',
             ])
